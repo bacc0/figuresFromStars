a0037e2e9a056a1027c9dc2b1081a801c21d8039
Veselin Hr. Trayanov <1.1@gmx.com>
2023-10-29 21:04:58 +0000

    comments added to the code

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+# Function to draw a right-angled triangle
 def draw_right_angle(n):
     for i in range(1, n + 1):
         print("*" * i)
 
+# Function to draw an upside-down right-angled triangle
 def draw_upside_down_right_angle(n):
     for i in range(n, 0, -1):
         print("*" * i)
 
+# Function to draw a pyramid
 def draw_pyramid(n):
     for i in range(1, n + 1):
         spaces = " " * (n - i)
@@ -14,7 +17,9 @@ def draw_pyramid(n):
         else:
             print(spaces + "*" + " " * (2 * i - 3) + "*")
 
+# Main function that interacts with the user
 def main():
+    # Prompt the user to choose a figure
     choice = input("_______________________________ "
                    "\nPlease Enter: "
                    "\n'a' for right angle,"
@@ -22,16 +27,20 @@ def main():
                    "\n'c' for a pyramid: "
                    "\n_______________________________ \n")
 
+    # Check if the choice is valid
     if choice not in ('a', 'b', 'c'):
         print("Invalid choice. Please select 'a', 'b', or 'c.")
         return
 
+    # Prompt the user for the number of rows
     n = int(input("Enter the number of rows (min 5): "))
 
+    # Check if the number of rows is at least 5
     if n < 5:
         print("Number of rows must be at least 5.")
         return
 
+    # Draw the selected figure based on the user's choice
     if choice == 'a':
         draw_right_angle(n)
     elif choice == 'b':

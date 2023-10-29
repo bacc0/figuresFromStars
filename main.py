65b8bf08e5496d2c6e10290474c76d77dfd63b43
Veselin Hr. Trayanov <1.1@gmx.com>
2023-10-29 21:11:22 +0000

    Goodbye message has been added

--- main.py
+++ main.py
@@ -19,34 +19,43 @@ def draw_pyramid(n):
 
 # Main function that interacts with the user
 def main():
-    # Prompt the user to choose a figure
-    choice = input("_______________________________ "
-                   "\nPlease Enter: "
-                   "\n'a' for right angle,"
-                   "\n'b' for upside-down right angle or "
-                   "\n'c' for a pyramid: "
-                   "\n_______________________________ \n")
+    while True:  # Add a loop for repeated figures
+        # Prompt the user to choose a figure
+        choice = input("_______________________________ "
+                       "\nPlease Enter: "
+                       "\n'a' for right angle,"
+                       "\n'b' for upside-down right angle or "
+                       "\n'c' for a pyramid: "
+                       "\n_______________________________ \n")
 
-    # Check if the choice is valid
-    if choice not in ('a', 'b', 'c'):
-        print("Invalid choice. Please select 'a', 'b', or 'c.")
-        return
+        # Check if the choice is valid
+        if choice not in ('a', 'b', 'c'):
+            print("Invalid choice. Please select 'a', 'b', or 'c.")
+            continue
 
-    # Prompt the user for the number of rows
-    n = int(input("Enter the number of rows (min 5): "))
+        # Prompt the user for the number of rows
+        n = int(input("Enter the number of rows (min 5): "))
 
-    # Check if the number of rows is at least 5
-    if n < 5:
-        print("Number of rows must be at least 5.")
-        return
+        # Check if the number of rows is at least 5
+        if n < 5:
+            print("Number of rows must be at least 5.")
+            continue
 
-    # Draw the selected figure based on the user's choice
-    if choice == 'a':
-        draw_right_angle(n)
-    elif choice == 'b':
-        draw_upside_down_right_angle(n)
-    elif choice == 'c':
-        draw_pyramid(n)
+        # Draw the selected figure based on the user's choice
+        if choice == 'a':
+            draw_right_angle(n)
+        elif choice == 'b':
+            draw_upside_down_right_angle(n)
+        elif choice == 'c':
+            draw_pyramid(n)
+
+        # Ask the user if they want to draw another figure or exit
+        another = input("Do you want to draw another figure? (y/n): ")
+        if another.lower() != 'y':  # Use 'y' for yes
+            print("_______________________________"
+                  "\n Goodbye!"
+                  "\n_______________________________")
+            break  # Exit the loop if the user does not want to draw another figure
 
 if __name__ == "__main__":
     main()
